37b28e067ee51ccdb8ad9b016ad710abfaac2b10
nlepage <nicolas.lepage@zenika.com>
2017-02-23 21:26:48 +0100

    tayo!

--- solve.js
+++ solve.js
@@ -6,7 +6,13 @@ const getCacheServersByEndpoint = require('./get-cache-servers-by-endpoint')
 module.exports = function solve (problem) {
   const requestByPopularity = _.orderBy(problem.requests, ['popularity'], ['desc'])
 
-  const cacheServers = _.uniqBy(_.map(_.flatMap(problem.endpoints, endpoint => endpoint.cacheServers), ({id}) => ({id, capacity: problem.cacheServerCapacity})), 'id')
+  const cacheServers = _.uniqBy(
+    _.map(
+      _.flatMap(problem.endpoints, endpoint => endpoint.cacheServers),
+      ({id}) => ({ id, capacity: problem.cacheServerCapacity, videos: [] })
+    ),
+    'id'
+  )
 
   let requestsWithCacheServer = _.map(requestByPopularity, request => {
     const video = _.find(problem.videos, {index: request.video})
@@ -14,13 +20,15 @@ module.exports = function solve (problem) {
     let cacheServersByEndPoint = getCacheServersByEndpoint(problem, request.endpoint)
     cacheServersByEndPoint = _.filter(cacheServersByEndPoint, ({id}) => {
       const cacheServer = _.find(cacheServers, {id})
-      return cacheServer && cacheServer.capacity >= video.size
+      return cacheServer && cacheServer.capacity >= video.size && !_.includes(cacheServer.videos, video.index)
     })
     cacheServersByEndPoint = _.sortBy(cacheServersByEndPoint, 'latency')
 
     const cacheServer = _.first(cacheServersByEndPoint)
     if (cacheServer) {
-      _.find(cacheServers, {id: cacheServer.id}).capacity -= video.size
+      const cacheServer2 = _.find(cacheServers, {id: cacheServer.id})
+      cacheServer2.capacity -= video.size
+      cacheServer2.videos.push(video.index)
       return {
         request,
         cacheServer
@@ -31,7 +39,7 @@ module.exports = function solve (problem) {
   const requestsByCacheServer = _.groupBy(requestsWithCacheServer, 'cacheServer.id')
   const solution = _.map(requestsByCacheServer, (requests, id) => ({
     id,
-    videos: _.uniq(_.map(requests, request => request.request.video))
+    videos: _.map(requests, request => request.request.video)
   }))
   return solution
 }
