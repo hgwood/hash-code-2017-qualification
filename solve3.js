b1c45b327929d35cf49fe837933833141c6d4ecf
Hugo Wood <hx4.5@free.fr>
2017-02-27 22:58:01 +0100

    wip

--- solve3.js
+++ solve3.js
@@ -34,9 +34,12 @@ module.exports = function solve (problem) {
     })
     // return caches.concat(_.map(endpoint.cacheServers, ({id, latency}) => ({id, latency, endpoint: endpoint.index})))
   }, {})
-  debug(c)
-  _.forEach(c, cache => x(cache.capacity, cache.videos))
-  process.exit()
+  // debug(c)
+  return _(c)
+    .mapValues(cache => y(cache.capacity, cache.videos))
+    .map((solution, id) => ({id, videos: _.map(solution.cachedVideos, 'id')}))
+    .value()
+  // process.exit()
 }
 
 function x (capacity, videos) {
@@ -57,16 +60,22 @@ function x (capacity, videos) {
   _.times(capacity + 1, i => {
     const bestVideo = _(videos)
       .filter(video => video.size <= i)
-      .map(video => ({id: video.id, size: video.size, score: scores[i - video.size] + video.score}))
+      .map(video => ({
+        id: video.id,
+        size: video.size,
+        score: scores[i - video.size] + video.score
+      }))
       .maxBy('score')
     scores[i] = _.get(bestVideo, 'score', scores[i])
-    refs[i] = bestVideo.size
+    refs[i] = bestVideo
+    videos = _.without(videos, bestVideo)
   })
 
-  const selectedVideos = []
+  const selectedVideos = [refs[refs.length - 1]]
   let j = refs.length - 1
   while (j > 0) {
-
+    j = refs[j - selectedVideos[0].size]
+    selectedVideos.unshift(refs[j])
   }
 
   // const scores = _.reduce(_.times(capacity + 1, i => 0), (scores, score, i) => {
@@ -77,5 +86,43 @@ function x (capacity, videos) {
   //   return scores
   // })
 
-  debug('score', scores[capacity])
+  debug('selectedVideos', selectedVideos)
+}
+
+function y (capacity, videos) {
+  const smallestVideo = _.minBy(videos, 'size')
+  videos = _(videos)
+    .filter(video => video.size <= capacity)
+    .keyBy('id')
+    .value()
+  const initialState = {
+    cachedVideos: [],
+    uncachedVideos: videos,
+    score: 0
+  }
+  const states = {0: initialState}
+  _.forEach(videos, video => {
+    states[video.size] = {
+      cachedVideos: [video],
+      uncachedVideos: _.omit(videos, video.id),
+      score: video.score
+    }
+  })
+  _.times(capacity + 1 - smallestVideo.size, i => {
+    if (!states[i]) return
+    const {cachedVideos, uncachedVideos, score} = states[i]
+    _(uncachedVideos)
+      .filter(video => video.size <= i)
+      .forEach(video => {
+        const existingState = states[i + video.size] || {score: 0}
+        const newState = {
+          cachedVideos: [...cachedVideos, video],
+          uncachedVideos: _.omit(uncachedVideos, video.id),
+          score: score + video.score
+        }
+        states[i + video.size] = _.maxBy([existingState, newState], 'score')
+      })
+  })
+  // debug(states)
+  return _(states).values().maxBy('score')
 }
